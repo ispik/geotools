d698bfab6ae382c089ff264aecbcd4d47717761a
Sebastian Graca <sgraca@poczta.onet.pl>
2012-04-02 22:51:58 +0200

    - make StreamingRenderer extensible - layer visibility based on scale - labels visibility based on layer configuration

--- modules/library/render/src/main/java/org/geotools/map/FeatureLayer.java
+++ modules/library/render/src/main/java/org/geotools/map/FeatureLayer.java
@@ -61,6 +61,10 @@ public class FeatureLayer extends StyleLayer {
     /** Listener to forward feature source events as layer events */
     protected FeatureListener sourceListener;
 
+    protected boolean drawLabels = true;
+    protected Double labelMinScaleDenominator;
+    protected Double labelMaxScaleDenominator;
+
     /**
      * Creates a new instance of FeatureLayer
      * 
@@ -183,6 +187,32 @@ public class FeatureLayer extends StyleLayer {
     public void setQuery(Query query) {
         this.query = query;
         fireMapLayerListenerLayerChanged(MapLayerEvent.FILTER_CHANGED);
+    }
+    
+    public boolean getDrawLabels() {
+        return drawLabels;
+    }
+    
+    public boolean getDrawLabelsAtScale(double scaleDenominator) {
+        if (labelMinScaleDenominator != null && scaleDenominator < labelMinScaleDenominator) {
+            return false;
+        }
+        if (labelMaxScaleDenominator != null && scaleDenominator > labelMaxScaleDenominator) {
+            return false;
+        }
+        return true;
+    }
+    
+    public void setDrawLabels(boolean drawLabels) {
+        this.drawLabels = drawLabels;
+    }
+    
+    public void setLabelMinScaleDenominator(Double labelMinScaleDenominator) {
+        this.labelMinScaleDenominator = labelMinScaleDenominator;
+    }
+    
+    public void setLabelMaxScaleDenominator(Double labelMaxScaleDenominator) {
+        this.labelMaxScaleDenominator = labelMaxScaleDenominator;
     }
 
     @Override
